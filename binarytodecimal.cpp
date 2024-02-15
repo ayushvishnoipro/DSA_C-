98f47bd6a2a3e0d1ad44aedc7b7d450a73a40596
Ayush Vishnoi <vishnoiayush39@gmail.com>
2024-02-16 00:17:02 +0530

    third commit

--- binarytodecimal.cpp
+++ binarytodecimal.cpp
@@ -1,29 +1,25 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int ans=0;
-    int i=0;
-    
+    cin >> n;
+    int ans = 0;
+    int i = 0;
+
     while (n != 0)
     {
         int bit = n % 10;
-        if (bit==1)
+        if (bit == 1)
         {
-           ans= ans+ pow(2,i) ;
+            ans = ans + pow(2, i);
         }
-        n=n/10;
-        
-        
-        
-        
+        n = n / 10;
+
         i++;
-      
     }
-    cout<<"the answer is="<<ans<<endl;
-    return 0;   
-    
+    cout << "the answer is=" << ans << endl;
+    return 0;
 }
